3696bb63d37d29ddef76370de955e8bf47c8a534
Sanaadak <sanaadk21@gmail.com>
2023-07-11 16:56:48 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,26 +6,23 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-
 char *str_concat(char *s1, char *s2)
-
 {
-
 	char *conct;
 	int i, ci;
 
 	if (s1 == NULL)
-
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
 
+	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
+
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
@@ -34,13 +31,12 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s1[i];
 		i++;
 	}
+
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
 		i++, ci++;
 	}
 	conct[i] = '\0';
-
 	return (conct);
 }
-
