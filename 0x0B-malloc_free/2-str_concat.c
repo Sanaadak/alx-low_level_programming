1b1dbc9275ad2d37ba23bb086987275c5d78a620
Sanaadak <sanaadk21@gmail.com>
2023-07-11 16:41:37 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
+	conct = malloc(size of(char) * (i + ci + 1));
 	if (conct == NULL)
 
 		return (NULL);
